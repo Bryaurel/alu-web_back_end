b06884ae6298ac583582e485374fae0f845615a0
Bryaurel <b.bakongobw@alustudent.com>
2024-10-03 03:40:51 +0200

    8-make_multiplier.py

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''
+type-annotated function make_multiplier that takes a float multiplier
+as argument and returns a function that multiplies a float by multiplier
+'''
+from typing import Callable
+# Callable is a type hint that indicates that an object
+# can be called like a function
+
+
+def make_multiplier(multiplier: float) -> Callable[[float]. float]:
+    '''
+    Create a multiplier
+    Callable[[float], float]: A function that multiplies a float by the multiplier
+    '''
+    def multiplier_function(value: float) -> float:
+        '''
+        Create a multiplier function
+        '''
+        return value * multiplier
+
+    return multiplier_function
