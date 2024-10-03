e88318230743af0e08e9ec4f53aa988fc9964b4d
Bryaurel <b.bakongobw@alustudent.com>
2024-10-03 02:52:31 +0200

    6-sum_mixed_list

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+type-annotated function sum_mixed_list which takes a list mxd_lst
+of integers and floats and returns their sum as a float.
+'''
+
+
+def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+    '''
+    return the sum of a mixed list of integers and floats
+    sum_mixed_list is the function name
+    '''
+    return sum_mixed_list(mxd_lst)
