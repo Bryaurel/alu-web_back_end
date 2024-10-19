db12df6e963a5bb7acf222c070b6407f21e6edbc
Bryaurel <b.bakongobw@alustudent.com>
2024-10-19 02:58:17 +0200

    encrypt_password.py

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+'''
+User passwords should NEVER be stored
+'''
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    expects one string argument name password
+    and returns a salted, hashed password, which is a byte string.
+    """
+    if password:
+        return bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check password
+
+    Args:
+        hashed_password (bytes): hash
+        password (str): password
+
+    Returns:
+        bool: true or false
+    """
+    if hashed_password and password:
+        return bcrypt.checkpw(str.encode(password), hashed_password)
