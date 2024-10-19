336836d94d55a03437b14901c08f9c14421f0af8
Bryaurel <b.bakongobw@alustudent.com>
2024-10-19 02:50:24 +0200

    filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+'''
+Write a function called filter_datum
+that returns the log message obfuscated
+'''
+
+import re
+
+def filter_datum(fields, redaction, message, separator):
+    '''
+    Returns the log message with fields obfuscated
+    '''
+    pattern = f"({'|'.join(fields)})=([^\\{separator}]+)"
+    return re.sub(pattern, lambda match: f"{match.group(1)}={redaction}", message)
